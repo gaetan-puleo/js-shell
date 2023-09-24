c4a2e6f6736bb6eb50860682953ef8d70252613b
Gaetan Puleo <pro@gaetanpuleo.com>
2023-09-24 21:27:26 +0200

    feat: remove tmux conf

--- flake.nix
+++ flake.nix
@@ -6,12 +6,12 @@
     devShell.x86_64-linux =
       let
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        FLAKE_PATH = "${self}";
       in pkgs.mkShell {
         buildInputs = [
           # Shell
           pkgs.tmux
           pkgs.ngrok
+          pkgs.fish
 
           # JS
           pkgs.bun
@@ -19,17 +19,7 @@
           pkgs.yarn
         ];
         shellHook = ''
-          echo "Entering shell"
-          FLAKE_PATH=${FLAKE_PATH}
-          alias nvim="nix run github:gaetan-puleo/nvim"
-          alias tmux="tmux -f '${FLAKE_PATH}/config/tmux/tmux.conf'  -f '${FLAKE_PATH}/config/tmux/theme.conf'"
-          export HOSTNAME=jsdevshell
-
-
-          PS1="\[\033[01;32m\]\u@\$HOSTNAME\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]$(parse_git_branch)\$ "
-
-          exec tmux -f '${FLAKE_PATH}/config/tmux/tmux.conf'  -f '${FLAKE_PATH}/config/tmux/theme.conf'
-
+          exec tmux
         '';
       };
       src = [
